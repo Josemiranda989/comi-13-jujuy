dab57d7e40c66ba954181f0ddf22b6884775a5ec
josemiranda989 <josemiranda989@gmail.com>
2023-12-13 12:11:02 -0300

    intro a html

---
+++ c17-intro-html/videoGames/app.js
@@ -0,0 +1,18 @@
+// Requires
+const express = require('express');
+const path = require('path')
+
+const app = express();
+const port = 3005
+
+
+// Config
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'home.html'))
+})
+
+app.listen(port, ()=>{
+    console.log(`Nuesta app corre en http://localhost:${port}`);
+})
