382c28509a25676053e59e25113bfa0d3a4a78f0
josemiranda989 <josemiranda989@gmail.com>
2024-05-09 20:49:51 -0300

    update

--- c54-hooks/GameOfThronesApp/src/components/GOT.jsx
+++ c54-hooks/GameOfThronesApp/src/components/GOT.jsx
@@ -1,9 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Card from "./Card";
 
 export default function GOT() {
   let [personajes, setPersonajes] = useState([]);
   let [numerito, setNumerito] = useState(0);
+
+  /*
+  // OPCION 2
+  let firstRender = useRef(true);
+  useEffect(() => {
+    if(firstRender.current){
+      firstRender.current = false;
+       fetch("https://thronesapi.com/api/v2/Characters")
+      .then((res) => res.json())
+      .then((data) => setPersonajes(data))
+      .catch((e) => console.log(e));
+      return;
+    } else {
+       fetch("https://thronesapi.com/api/v2/Characters")
+      .then((res) => res.json())
+      .then((data) => setPersonajes(data.filter((p) => p.family === 'House Targaryen')))
+      .catch((e) => console.log(e))
+    }   
+  }, [numerito]); */
 
   useEffect(() => {
     fetch("https://thronesapi.com/api/v2/Characters")
